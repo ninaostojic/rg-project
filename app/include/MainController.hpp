fc6dc0afa07d04ec8a57752b53aa7c2e1014fea1
ninaostojic <ostojic.nina99@gmail.com>
2025-02-12 17:14:21 +0100

    Dodati svi fajlovi

--- app/include/MainController.hpp
+++ app/include/MainController.hpp
@@ -11,13 +11,23 @@ namespace app {
 class MainController : public engine::core::Controller{
     void initialize() override;
 
+    bool loop() override;
+
+    static void draw_corgi();
+
+    void begin_draw() override;
+
+    void draw() override;
+
+    void end_draw() override;
+
 
 public:
     std::string_view name() const override {
         return "app::MainController";
     }
 
-    bool loop();
+
 };
 
 } // app
--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -2,12 +2,17 @@
 // Created by linux on 2/6/25.
 //
 
-#include "../include/MainController.hpp"
 #include <engine/core/Engine.hpp>
+#include <engine/graphics/GraphicsController.hpp>
+#include "../include/MainController.hpp"
+
 #include <spdlog/spdlog.h>
 
 namespace app {
     void MainController::initialize() {
+        engine::graphics::OpenGL::enable_depth_testing();
+
+
         spdlog::info("MainController initialized");
     }
 
@@ -18,7 +23,38 @@ namespace app {
         }
         return true;
     }
+    void MainController::draw_corgi() {
+
+        // Model
+        auto resources                  = engine::core::Controller::get<engine::resources::ResourcesController>();
+        auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+        engine::resources::Model* corgi = resources->model("corgi");
+        // Shader
+        engine::resources::Shader* shader = resources->shader("basic");
+
+        shader->use();
+        shader->set_mat4("projection", graphics->projection_matrix());
+        shader->set_mat4("view", graphics->camera()->view_matrix());
+        auto model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
+        model = glm::scale(model, glm::vec3(0.3f));
+        shader->set_mat4("model", model);
 
 
+        corgi->draw(shader);
+    }
+    void MainController::begin_draw() {
+
+        engine::graphics::OpenGL::clear_buffers();
+    }
+    void MainController::draw() {
+        // clear buffers (color buffer, depth buffer)
+        draw_corgi();
+        // swapbuffers
+    }
+    void MainController::end_draw() {
+        auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+        platform->swap_buffers();
+    }
 
 } // app
