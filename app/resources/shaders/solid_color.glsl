3e2b8e3fc67d0fc0efc7facef1578195c8057c08
ninaostojic <ostojic.nina99@gmail.com>
2025-10-09 14:20:11 +0200

    Dodat jednobojni shader

---
+++ app/resources/shaders/solid_color.glsl
@@ -0,0 +1,25 @@
+//#shader vertex
+#version 330 core
+
+layout (location = 0) in vec3 aPos;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main()
+{
+    vec3 FragPos = vec3(model * vec4(aPos, 1.0));
+    gl_Position = projection * view * vec4(FragPos, 1.0);
+}
+
+//#shader fragment
+#version 330 core
+
+layout (location = 0) out vec4 FragColor;
+layout (location = 1) out vec4 BrightColor;
+
+uniform vec4 color;
+void main() {
+    FragColor = color;
+}
