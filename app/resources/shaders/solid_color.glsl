13e5804e5c012a40becf43db43b8a34a9d5a1ee6
ninaostojic <ostojic.nina99@gmail.com>
2025-10-09 15:08:51 +0200

    Stari shaderi sada podrzavaju bloom

--- app/resources/shaders/solid_color.glsl
+++ app/resources/shaders/solid_color.glsl
@@ -22,4 +22,10 @@ layout (location = 1) out vec4 BrightColor;
 uniform vec4 color;
 void main() {
     FragColor = color;
+    float brightness = dot(FragColor.rgb, vec3(0.2126, 0.7152, 0.0722));
+    if (brightness > 1.0) {
+       BrightColor = vec4(FragColor.rgb, 1.0);
+    } else {
+       BrightColor = vec4(0.0, 0.0, 0.0, 1.0);
+    }
 }
