52fc5a41399a331bc645e0f03d11b05b7aa21961
ninaostojic <ostojic.nina99@gmail.com>
2025-10-11 10:34:47 +0200

    Dodat poseban instancing shader

--- app/resources/shaders/textured_lit_instanced.glsl
+++ app/resources/shaders/textured_lit_instanced.glsl
@@ -4,6 +4,7 @@
 layout(location = 0) in vec3 aPos;
 layout(location = 1) in vec3 aNormal;
 layout(location = 2) in vec2 aTexCoords;
+layout(location = 5) in vec3 aOffset;
 
 out vec2 TexCoords;
 out vec3 Normal;
@@ -15,7 +16,7 @@ uniform mat4 projection;
 
 void main()
 {
-    FragPos = vec3(model * vec4(aPos, 1.0));
+    FragPos = vec3(model * vec4(aPos, 1.0)) + aOffset;
     Normal = aNormal;
     TexCoords = aTexCoords;
     gl_Position = projection * view * vec4(FragPos, 1.0);
